1f24f7ac68e5e1dbca4519cc7c93bc870b3ea009
Szabolcs Nagy <48963820+lulwszabolcs@users.noreply.github.com>
2022-10-07 08:23:32 +0200

    kutya makcsa teknos

---
+++ JavaGyak12.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class JavaGyak12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        /*1
+        String s1 = sc.nextLine();
+        char c1 = sc.next().charAt(0);
+        char c2 = sc.next().charAt(0);
+        System.out.println(s1.replace(c1,c2));
+        //2
+        int szam1 = sc.nextInt();
+        int szam2 = sc.nextInt();
+        System.out.println(szam1/szam2);
+        //3
+        String szo3 = sc.nextLine();
+        String szo4 = sc.nextLine();
+        System.out.println(szo3.equals(szo4));
+        //4
+        byte b1 = sc.nextByte();
+        byte b2 = sc.nextByte();
+        System.out.println(b1 | b2);
+        //5
+        byte b3 = sc.nextByte();
+        byte b4 = sc.nextByte();
+        System.out.println(b3 >> b4);
+        //6
+        String szo5 = sc.nextLine();
+        System.out.println(szo5.endsWith("a"));
+        //7
+        String szo6 = sc.nextLine();
+        System.out.println(szo6.lastIndexOf("e")-1);
+        //8
+        String szo7 = sc.nextLine();
+        short s1 = sc.nextShort();
+        short s2 = sc.nextShort();
+        System.out.println(szo7.substring(s1,s2));
+        //9
+        float f1 = sc.nextFloat();
+        float f2 = sc.nextFloat();
+        System.out.println(f1 > f2 ? "f1 nagyobb" : "f2 nagyobb");
+        // 2 string ameliykbe elobb van a betu
+        String szo8 = sc.nextLine();
+        String szo9 = sc.nextLine();
+        System.out.println(szo8.indexOf("a") < szo9.indexOf("a") ? "Elsoben van elobb" : "Masodikban van elobb");
+        //3 string ha valamelyikben van space irja ki hogy van, ha nincs akkor nincs
+        String sz1 = sc.nextLine();
+        String sz2 = sc.nextLine();
+        String sz3 = sc.nextLine();
+        System.out.println(sz1.contains(" ") | sz2.contains(" ") | sz3.contains(" ") ? "Van" : "Nincs");*/
+        String sz3 = sc.nextLine();
+        String sz4 = sc.nextLine();
+        int i1 = sc.nextInt();
+        int i2 = sc.nextInt();
+        System.out.println(sz3+sz4);
+    }
+}
