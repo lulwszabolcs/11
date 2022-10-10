00d2708126a1a847aff369d8e4701b745cbc4230
Szabolcs Nagy <48963820+lulwszabolcs@users.noreply.github.com>
2022-10-10 11:09:23 +0200

    kutya macska teknos

--- JavaGyak12.java
+++ JavaGyak12.java
@@ -3,55 +3,21 @@ import java.util.Scanner;
 public class JavaGyak12 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        /*1
-        String s1 = sc.nextLine();
-        char c1 = sc.next().charAt(0);
-        char c2 = sc.next().charAt(0);
-        System.out.println(s1.replace(c1,c2));
-        //2
-        int szam1 = sc.nextInt();
-        int szam2 = sc.nextInt();
-        System.out.println(szam1/szam2);
-        //3
-        String szo3 = sc.nextLine();
-        String szo4 = sc.nextLine();
-        System.out.println(szo3.equals(szo4));
-        //4
+        /*String s1 = sc.nextLine();
+        //System.out.println(s1.length() % 2 == 0 ? s1.substring(s1.length()/2-1,s1.length()/2+1) : s1.substring(s1.length()/2-1,s1.length()/2+2)));
+        String s2 = sc.nextLine();
+        System.out.println(s2.contains("a") ? s2.substring(s2.indexOf("a"),s2.indexOf("a")+2) : "Nem talalhato");
+        String s3 = "   ";
+        System.out.println(s3.isBlank());
+        System.out.println(s3.isEmpty()); */
+        short sh1 = sc.nextShort();
+        short sh2 = sc.nextShort();
+        System.out.println(++sh1 + "," + ++sh2);
         byte b1 = sc.nextByte();
         byte b2 = sc.nextByte();
-        System.out.println(b1 | b2);
-        //5
-        byte b3 = sc.nextByte();
-        byte b4 = sc.nextByte();
-        System.out.println(b3 >> b4);
-        //6
-        String szo5 = sc.nextLine();
-        System.out.println(szo5.endsWith("a"));
-        //7
-        String szo6 = sc.nextLine();
-        System.out.println(szo6.lastIndexOf("e")-1);
-        //8
-        String szo7 = sc.nextLine();
-        short s1 = sc.nextShort();
-        short s2 = sc.nextShort();
-        System.out.println(szo7.substring(s1,s2));
-        //9
-        float f1 = sc.nextFloat();
-        float f2 = sc.nextFloat();
-        System.out.println(f1 > f2 ? "f1 nagyobb" : "f2 nagyobb");
-        // 2 string ameliykbe elobb van a betu
-        String szo8 = sc.nextLine();
-        String szo9 = sc.nextLine();
-        System.out.println(szo8.indexOf("a") < szo9.indexOf("a") ? "Elsoben van elobb" : "Masodikban van elobb");
-        //3 string ha valamelyikben van space irja ki hogy van, ha nincs akkor nincs
-        String sz1 = sc.nextLine();
-        String sz2 = sc.nextLine();
-        String sz3 = sc.nextLine();
-        System.out.println(sz1.contains(" ") | sz2.contains(" ") | sz3.contains(" ") ? "Van" : "Nincs");*/
-        String sz3 = sc.nextLine();
-        String sz4 = sc.nextLine();
-        int i1 = sc.nextInt();
-        int i2 = sc.nextInt();
-        System.out.println(sz3+sz4);
+        System.out.println(b1 ^ b2);
+        String szo1 = sc.nextLine();
+        char c1 = sc.next().charAt(0);
+        System.out.println(szo1.replace());
     }
 }
