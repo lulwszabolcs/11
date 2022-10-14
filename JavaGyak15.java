d604f2133a9abb374e0cece6804c1fed48f23ff0
Szabolcs Nagy <48963820+lulwszabolcs@users.noreply.github.com>
2022-10-14 08:23:14 +0200

    Add files via upload

---
+++ JavaGyak15.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class JavaGyak15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        /*
+        double r = sc.nextDouble();
+        System.out.println("Kerulete: " + " "+ 2*r*Math.PI);
+        System.out.println("Terulete: " + r*r*Math.PI);
+        int num1 = sc.nextInt();
+        System.out.println(Math.abs(num1));
+        System.out.println(num1 < 0 ? num1 * -1 : num1);
+
+        System.out.println(Math.ceil(5.01));
+        System.out.println(Math.cos(0));
+        System.out.println(Math.floor(5.3));
+        System.out.println(Math.max(10,11));
+        byte n2 = sc.nextByte();
+        byte n3 = sc.nextByte();
+        System.out.println(Math.max(n2,n3));
+
+
+        byte n4 = sc.nextByte();
+        byte n5 = sc.nextByte();
+        System.out.println(Math.min(n4,n5));
+        System.out.println(Math.pow(2,10));
+
+        byte b1 = sc.nextByte();
+        byte b2 = sc.nextByte();
+        System.out.println(Math.pow(b1,b2));
+
+        System.out.println(Math.floor(Math.random()*10)+1);
+        System.out.println(Math.round(5.4));
+        System.out.println(Math.sqrt(36));
+
+        byte b3 = sc.nextByte();
+        byte b4 = sc.nextByte();
+        System.out.println(Math.max(Math.abs(b3),Math.abs(b4)));
+
+        int velszam1 = (int)(Math.floor(Math.random()*10)+1);
+        int velszam2 = (int)(Math.floor(Math.random()*10)+1);
+        System.out.println(velszam1 + " " + velszam2);
+        System.out.println(Math.min(velszam1,velszam2));
+
+        int velszam3 = (int)(Math.floor(Math.random()*150)+50);
+        System.out.println(velszam3);
+         */
+        byte b5 = sc.nextByte();
+        System.out.println(Math.sqrt(b5));
+    }
+}
