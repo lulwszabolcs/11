701fb3b4078ad223734d0200fea3ec43aef75357
Szabolcs Nagy <48963820+lulwszabolcs@users.noreply.github.com>
2022-10-17 11:07:49 +0200

    kutya macska teknos

---
+++ JavaGyak16.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class JavaGyak16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        /*
+        float f1 = sc.nextFloat();
+        float f2 = sc.nextFloat();
+        System.out.println(Math.min(f1,f2));
+
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        System.out.println(Math.pow(n1,n2));
+
+        System.out.println(Math.sqrt(n2));
+
+        System.out.println("-10 abszolut erteke: " + Math.abs(-10));
+
+        System.out.println(Math.floor((Math.random()*90)+1));
+*/
+        System.out.println(Math.floor(Math.random()*5)+5);
+        System.out.println(Math.ceil(Math.random()*20 + 10));
+        System.out.println(Math.round((Math.random()*20) -10));
+        /*
+        int n3 = sc.nextInt();
+        int n4 = sc.nextInt();0
+        System.out.println(Math.max(Math.abs(n3),Math.abs(n4)));
+        */
+
+    }
+}
