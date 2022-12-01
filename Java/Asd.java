e816ae5237b400bf28a42360f606bf9e3bc6b124
Szabolcs Nagy <48963820+lulwszabolcs@users.noreply.github.com>
2022-12-01 11:57:26 +0100

    Add files via upload

---
+++ Java/Asd.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class Asd {
+    public static void main(String[] args) {
+        /*
+        int a1 = 1;
+        Integer a2 = 2;
+        a2 = a1;
+        Integer sum = 0;
+        int[] szamok = {1,2,3,4,5,6,7,8,9,10};
+        for (int i = 0; i < szamok.length; i++) {
+            if (i % 2 == 0) {
+                sum+=i;
+            }
+        }
+        System.out.println(sum);
+        */
+        Boolean b1 = true;
+        Character c1 = 'X';
+        System.out.println(c1.charValue());
+    }
+}
