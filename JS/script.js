9e0cf9967ebefa153e9f7c2a2a5b8feaf79804a5
Szabolcs Nagy <48963820+lulwszabolcs@users.noreply.github.com>
2023-02-15 16:40:25 +0100

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -10,3 +10,24 @@ function kep() {
         document.getElementById("kephelye").src = "szomoru.jpg"
     }
 }
+let evszakhelye = document.getElementById("evszakhelye");
+let tel = document.getElementById("tel");
+let tavasz = document.getElementById("tavasz");
+let nyar = document.getElementById("nyar");
+let osz = document.getElementById("osz");
+tel.addEventListener("click", evszak);
+tavasz.addEventListener("click", evszak);
+nyar.addEventListener("click", evszak);
+osz.addEventListener("click", evszak);
+
+function evszak() {
+    if (tel.checked) {
+        evszakhelye.src = "tel.jpg"
+    } else if (tavasz.checked) {
+        evszakhelye.src = "tavasz.jpg"
+    } else if (nyar.checked) {
+        evszakhelye.src = "nyar.jpg"
+    } else {
+        evszakhelye.src = "osz.jpg"
+    }
+}
